4da8430d27b04b09881a233b19b78df23d12b3e9
Kurt Jacobs <89914869+kurtenjoybloom@users.noreply.github.com>
2022-03-22 15:07:18 +0200

    move into correct place to include

--- Source/Charts/Utils/Array+Access.swift
+++ Source/Charts/Utils/Array+Access.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Collection {
+extension Swift.Collection {
     subscript (safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
     }
